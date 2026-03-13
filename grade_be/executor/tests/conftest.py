2575903a6b5258d4e9b8757495b914cf6e14e344
vishnupreethi05 <23z278@psgtech.ac.in>
2026-03-13 22:41:22 +0530

    changes in be and fe

--- grade_be/executor/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-@pytest.fixture(autouse=True)
-def set_dummy_api_keys(settings):
-    settings.GEMINI_API_KEY = "dummy-gemini-key"
-    settings.COHERE_API_KEY = "dummy-cohere-key"
--- grade_be/waitress_test.py
+++
@@ -1,9 +0,0 @@
-
-from waitress import serve
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b"Hello from Waitress!"]
-
-if __name__ == "__main__":
-    print("Serving on port 8000...")
-    serve(app, host='127.0.0.1', port=8000)
